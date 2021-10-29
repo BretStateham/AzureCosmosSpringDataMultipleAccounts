93cc84dde7ba3ba9b4de632d213ece4524cfe9f1
Bret Stateham <Bret.Stateham@microsoft.com>
2021-10-29 08:30:28 -0700

    Create spring boot web app

---
+++ src/main/java/bs/sample/cdbma/CdbmaApplication.java
@@ -0,0 +1,12 @@
+package bs.sample.cdbma;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdbmaApplication {
+    public static void main(String[] args){
+        SpringApplication.run(CdbmaApplication.class,args);
+    }
+}
