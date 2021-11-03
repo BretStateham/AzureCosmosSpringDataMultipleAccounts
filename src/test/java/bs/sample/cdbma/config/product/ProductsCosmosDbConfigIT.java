46f5e2cf5c18d15512cfeb928fa56c6919094ed3
Bret Stateham <Bret.Stateham@microsoft.com>
2021-11-03 10:22:49 -0700

    Complete tests

--- src/test/java/bs/sample/cdbma/config/product/ProductsCosmosDbConfigIT.java
+++ src/test/java/bs/sample/cdbma/config/product/ProductsCosmosDbConfigIT.java
@@ -1,18 +1,14 @@
-package bs.sample.cdbma.config.person;
+package bs.sample.cdbma.config.product;
 
-import com.azure.spring.data.cosmos.config.CosmosConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.TestPropertySource;
-
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class PeopleCosmosDbConfigIT {
+public class ProductsCosmosDbConfigIT {
 
     @Autowired
     private ApplicationContext applicationContext;
@@ -25,6 +21,7 @@ class PeopleCosmosDbConfigIT {
 
     @Test
     void cosmosConfigBeanExists(){
-        assertNotNull(applicationContext.getBean("peopleCosmosConfig"));
+        assertNotNull(applicationContext.getBean("productsCosmosConfig"));
     }
+
 }
